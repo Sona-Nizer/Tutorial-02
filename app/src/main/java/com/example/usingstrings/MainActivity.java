5a60b5bdd0e9ce486a6dea293ace0adafdd2e4b8
Sona-Nizer <68426609+Sona-Nizer@users.noreply.github.com>
2020-08-04 18:07:46 +0530

    Initial commit

--- app/src/main/java/com/example/usingstrings/MainActivity.java
+++ app/src/main/java/com/example/usingstrings/MainActivity.java
@@ -3,11 +3,13 @@ package com.example.usingstrings;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView TvMsg2;
+    private static final String TAG = "Lifecycle";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +17,36 @@ public class MainActivity extends AppCompatActivity {
 
         TvMsg2 =findViewById(R.id.TvMsg2);
         TvMsg2.setText(R.string.Msg2);
+
+        Log.i(TAG, "onCreate: ");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, "onStart: ");
+
+    }
+
+    @Override
+    protected void onStop() {
+
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onPostResume() {
+
+        super.onPostResume();
+        Log.i(TAG, "onPostResume: ");
+    }
+
+    @Override
+    protected void onRestart()
+    {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
     }
 }
